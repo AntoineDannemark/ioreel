6bcd900d660fbab4963f419d86288b64a32b5218
Antoine <antoine.dannemark@protonmail.com>
2021-01-26 11:43:10 +0100

    install typeorm for mobile & electron

--- src/App.jsx
+++ src/App.jsx
@@ -30,6 +30,8 @@ import "@ionic/react/css/display.css";
 /* Theme variables */
 import "./theme/variables.css";
 
+import "reflect-metadata";
+
 const Router = isPlatform("electron")
     ? IonReactHashRouter
     : IonReactRouter;
