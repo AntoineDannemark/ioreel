73231c82428b90a437c3baee5d0cfbfe77b315ec
AntoineDannemark <antoine.dannemark@protonmail.com>
2021-02-24 16:23:03 +0100

    pass env.IS_SLS to react

--- config/env.js
+++ config/env.js
@@ -1,4 +1,4 @@
-'use strict';
+
 
 const fs = require('fs');
 const path = require('path');
@@ -85,6 +85,7 @@ function getClientEnvironment(publicUrl) {
         WDS_SOCKET_HOST: process.env.WDS_SOCKET_HOST,
         WDS_SOCKET_PATH: process.env.WDS_SOCKET_PATH,
         WDS_SOCKET_PORT: process.env.WDS_SOCKET_PORT,
+        IS_SLS: process.env.IS_SLS,
       }
     );
   // Stringify all values so we can feed into webpack DefinePlugin
