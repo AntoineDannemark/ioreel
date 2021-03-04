2c30c465ffed4f514811e4f174c448c80d68f52f
Antoine <antoine.dannemark@protonmail.com>
2021-03-04 08:16:23 +0100

    Fix units tab nav

--- src/pages/Units.tsx
+++ src/pages/Units.tsx
@@ -1,7 +1,14 @@
+import { IonContent, IonPage } from "@ionic/react";
 import React from "react";
 
 const Units: React.FC<any> = () => {
-  return <div>UNITS</div>;
+  return (
+    <IonPage>
+      <IonContent>
+        <div>UNITS</div>
+      </IonContent>
+    </IonPage>
+  );
 };
 
 export default Units;
