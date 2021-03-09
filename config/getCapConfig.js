be7066d752bd9eb7f8d3f05498a2edc27764825d
Antoine <antoine.dannemark@protonmail.com>
2021-03-09 09:39:39 +0100

    getLocalIP + use in getCapConfig

--- config/getCapConfig.js
+++ config/getCapConfig.js
@@ -1,26 +1,36 @@
 // Script to generate different capacitor configuration for dev or prod,
 // defining a server ip and port when in dev mode, to enable live-reloading
-// Stolen from :
-// https://gist.github.com/damienromito/e6b3930ffde4c7240f109d1de69febb5
+(async () => {
+    const chalk = require('chalk');
 
-const ip = require('./getLocalIp')();
+    const fs = require('fs')
+    const path = require('path')
 
-const fs = require('fs')
-const path = require('path')
+    const configPath = path.resolve(__dirname, '../capacitor.config.json')
 
-const configPath = path.resolve(__dirname, '../capacitor.config.json')
+    let rawdata = fs.readFileSync(configPath)
+    let capacitorConfig = JSON.parse(rawdata)
 
-let rawdata = fs.readFileSync(configPath)
-let capacitorConfig = JSON.parse(rawdata)
+    const arg = process.argv[2];
 
-const arg = process.argv[2]
-if(arg === 'dev'){
-  capacitorConfig.server = {
-    url: "http://" + ip + ":8100",
-    cleartext: true
-  }
-}else{
-  delete capacitorConfig.server
-}
+    if(arg === 'dev'){
+        try {
+            const ip = await require('./getLocalIp')();
 
-fs.writeFileSync(configPath, JSON.stringify(capacitorConfig, null, 2))
+            capacitorConfig.server = {
+                url: "http://" + ip + ":8100",
+                cleartext: true
+            }
+            console.log(chalk.bold.yellow('[INFO]  @ getCapConfig - Capacitor will be listening for changes on port 8100'));
+        } catch(err) {
+           process.exitCode = 1;
+            process.exit()
+        }
+
+    } else {
+        console.log(chalk.bold.yellow('[INFO] @ getCapConfig - Capacitor set on production mode - live-reload disabled'));
+        delete capacitorConfig.server
+    }
+
+    return fs.writeFileSync(configPath, JSON.stringify(capacitorConfig, null, 2))
+})()
--- config/getLocalIp.js
+++ config/getLocalIp.js
@@ -1,6 +1,8 @@
 const { networkInterfaces } = require('os');
+const chalk = require('chalk');
+const { Select } = require('enquirer');
 
-module.exports = () => {
+module.exports = async() => {
     const nets = networkInterfaces();
     const results = {};
     
@@ -16,7 +18,42 @@ module.exports = () => {
             }
         }
     }
-    
-    return results['Ethernet'] ? results['Ethernet'] : results['WiFi']
+
+    let localIP;
+
+    switch(Object.keys(results).length) {
+        // If no result, notify the user & error out 
+        case(0):
+            console.log(chalk.bold.red('[ERROR] @ getLocalIP - No local IP found, please check your connection'));          
+            throw new Error('No local IP found, please check your connection');
+        // If only one result, use it
+        case(1):
+            console.log(chalk.bold.green(`[SUCCESS] @ getLocalIP - App build will be served on your local network on IP ${Object.values(results)[0]} (${Object.keys(results)[0]})`))
+            localIP = Object.values(results)[0]
+            break; 
+        // Else ask the user to select
+        default: 
+            const prompt = new Select({
+                name: 'ip',
+                message: 'We found more than one usable IP, please select:',
+                choices: Object.entries(results).map(result => ({
+                    name: result[1],
+                    message: `${result[1]} - (${result[0]})`,
+                }))
+            });
+            
+            await prompt.run()
+                .then(ip => {
+                    console.log(chalk.bold.green(`[SUCCESS] @ getLocalIP - App build will be served on your local network on IP ${ip} (${Object.keys(results).filter(key => results[key] === ip)})`));
+                    localIP = ip
+                })
+                .catch(err => {
+                    console.log(chalk.bold.red(`[ERROR] @ getLocalIP - No IP selected`));
+                    throw new Error('No IP selected');
+                });
+            break;
+    }
+
+    return localIP
 }
 
