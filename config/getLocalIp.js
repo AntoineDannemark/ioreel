4c434214dd3a87875a1446a53c2ac79e1ece6508
AntoineDannemark <antoine.dannemark@protonmail.com>
2021-03-09 15:46:21 +0100

    getLocalIP - Fix no filter on net name

--- config/getLocalIp.js
+++ config/getLocalIp.js
@@ -7,10 +7,6 @@ module.exports = async() => {
     const results = {};
     
     for (const name of Object.keys(nets)) {
-        if (!name.includes('Ethernet') && !name.includes('WiFi')) {
-            continue;
-        }
-    
         for (const net of nets[name]) {
             // Skip over non-IPv4 and internal (i.e. 127.0.0.1) addresses
             if (net.family === 'IPv4' && !net.internal) {            
