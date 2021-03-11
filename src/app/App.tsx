dd6751fb90b8bbaa68ca65be27fdaa4bd5684e4d
AntoineDannemark <antoine.dannemark@protonmail.com>
2021-03-11 14:08:19 +0100

    @App.tsx remove useless Redirect import

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { Redirect, Route } from "react-router-dom";
+import { Route } from "react-router-dom";
 import ProtectedRoute, {
   ProtectedRouteProps,
 } from "../features/User/ProtectedRoute";
